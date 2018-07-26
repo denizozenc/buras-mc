d3af9a9109b9f49fb25ef91bf9fcdd5b801f4ba0
denizozenc <39837176+denizozenc@users.noreply.github.com>
2018-07-26 12:39:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -80,7 +80,7 @@ client.unload = command => {
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
-    msg.reply('**Aleyküm Selam Güzel Kardeşim ** :fire:  ');
+    msg.reply('**https://discord.gg/pezVTj** :fire:  ');
   }
   
 });
