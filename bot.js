fb786fb220a7e0351899d6ac67ad8a5e21b2dc75
denizozenc <39837176+denizozenc@users.noreply.github.com>
2018-07-26 12:45:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -80,7 +80,7 @@ client.unload = command => {
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
-    msg.reply('k!ban Polαт Erdeм | Aslıvar ARMY dolandırıcılık :fire:  ');
+    msg.reply(' https://discord.gg/pezVTj  ');
   }
   
 });
